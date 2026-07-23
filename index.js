e99e1679aaa84cf7913ec97ce8d2560a60a2c4cc
016Naphatsorn <674259016@webmail.npru.ac.th>
2026-07-23 15:49:18 +0700

    end23/07/26

--- index.js
+++ index.js
@@ -1,85 +1,112 @@
 import express from "express";
-import {Product,connectDB} from "./db.js";
-//create server
+import { Product, connectDB } from "./db.js";
 const app = express();
 const PORT = 5000;
 
-//middleware อ่าน  json จาก request body
 app.use(express.json());
 
-connectDB(); //connect to database
-let products = [
-  { id: 1, name: "Laptop", price: 29900 },
-  { id: 2, name: "SmartPhone", price: 39900 },
-  { id: 3, name: "Tablet", price: 19900 },
-];
+connectDB();
 
-//Routing
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to my first Restful API");
+  return res.json(products);
 });
 
-//get all product
-app.get("/api/products", (req, res) => {
-  return res.status(200).json(products);
-});
-
-//get product by id
-app.get("/api/products/:id", (req, res) => {
-  const productId = Number(req.params.id);
-  const product = products.find((p) => p.id === productId);
-  if (!product) {
-    return res.status(404).json({ message: "Product not found" });
+// สร้าง Product
+app.post("/api/products", async (req, res) => {
+  try {
+    const { name, price } = req.body;
+    if (!name || !price) {
+      return res
+        .status(400)
+        .json({ message: "Name & Price are required fields!!" });
+    }
+    const newProduct = await Product.create({
+      name: name,
+      price: Number(price),
+    });
+    return res.status(201).json(newProduct);
+  } catch (error) {
+    console.error("Server error!", error);
+    return res.status(500).json({ error: error.message });
   }
-  return res.status(200).json(product);
 });
 
-//create new product
-app.post("/api/products", (req, res) => {
-  const { name, price } = req.body;
-  if (!name || !price) {
-    return res.status(400).json({ massage: "Name and price are required!" });
+// ดูทั้งหมด
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    return res.json(products);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
-  const newProduct = {
-    id: products.length > 0 ? Math.max(...products.map((p) => p.id)) + 1 : 1,
-    name: name,
-    price: Number(price),
-  };
-  products.push(newProduct);
-  return res.status(201).json(newProduct);
 });
 
-//update product by id
-app.put("/api/products/:id", (req, res) => {
-  const { name, price } = req.body;
-  const productId = Number(req.params.id);
-  const productsIndex = products.findIndex((p) => p.id === productId);
-  if (productsIndex === -1) {
-    return res.status(404).json({ message: "Product not found" });
+//find by Id
+
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400)({ message: "Id needed!" });
+    }
+    const product = await Product.findByPk(id);
+    if (!product) {
+      return res.status(404)({ message: "Product not Found!" });
+    }
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
-  products[productsIndex] = {
-    id: productId,
-    name: name || products[productsIndex].name,
-    price: Number(price) || products[productsIndex].price,
-  };
-  return res.status(200).json(products[productsIndex]);
 });
 
-//delete product by id
-app.delete("/api/products/:id", (req, res) => {
-  const productId = Number(req.params.id);
-  const productsIndex = products.findIndex((p) => p.id === productId);
-  if (productsIndex === -1) {
-    return res.status(404).json({ message: "Product not found" });
+// อัพเดท Product
+app.put("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ message: "Id needed!" });
+    }
+    const { name, price } = req.body;
+    if (!name && !price) {
+      return res
+        .status(400)
+        .json({ message: "Name & Price are required fields!!" });
+    }
+    const product = await Product.findByPk(id);
+    if (!product) {
+      return res.status(404)({ message: "Product not Found!" });
+    }
+    await product.update({
+      name: name || product.name,
+      price: Number(price) || product.price,
+    });
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
-  const deleteProduct = products.splice(productsIndex, 1);
-  return res.status(200).json({
-    message: "Product delete successfully",
-    deleteProduct: deleteProduct[0],
-  });
 });
 
-// ดักฟัง request
+//Delete product
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ message: "Id needed!" });
+    }
+    const product = await Product.findByPk(id);
+    if (!product) {
+      return res.status(404)({ message: "Product not Found!" });
+    }
+    await product.destroy();
+    return res.status(200).json({
+      massage: "product is deleted successfully",
+      deleteProduct: product,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server is running on: http://localhost:${PORT}`);
 });
